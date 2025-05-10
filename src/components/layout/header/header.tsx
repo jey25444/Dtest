1f3cb3929d0c104eae368046c67260c2ab3a2112
jey25444 <tuty6734@gmail.com>
2025-05-10 10:44:49 +0300

    Update header.tsx

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -26,170 +26,168 @@ import { useState } from 'react';
 import { Menu } from 'lucide-react';
 
 const AppHeader = observer(() => {
-    const { isGBLoaded, isGBAvailable } = useIsGrowthbookIsLoaded();
-    const { isDesktop } = useDevice();
-    const { isAuthorizing, activeLoginid } = useApiBase();
-    const { client } = useStore() ?? {};
+    const { isGBLoaded, isGBAvailable } = useIsGrowthbookIsLoaded();
+    const { isDesktop } = useDevice();
+    const { isAuthorizing, activeLoginid } = useApiBase();
+    const { client } = useStore() ?? {};
 
-    const { data: activeAccount } = useActiveAccount({ allBalanceData: client?.all_accounts_balance });
-    const { accounts, getCurrency } = client ?? {};
-    const has_wallet = Object.keys(accounts ?? {}).some(id => accounts?.[id].account_category === 'wallet');
+    const { data: activeAccount } = useActiveAccount({ allBalanceData: client?.all_accounts_balance });
+    const { accounts, getCurrency } = client ?? {};
+    const has_wallet = Object.keys(accounts ?? {}).some(id => accounts?.[id].account_category === 'wallet');
 
-    const currency = getCurrency?.();
-    const { localize } = useTranslations();
+    const currency = getCurrency?.();
+    const { localize } = useTranslations();
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const renderAccountSection = () => {
-        if (isAuthorizing) {
-            return <AccountsInfoLoader isLoggedIn isMobile={!isDesktop} speed={3} />;
-        } else if (activeLoginid) {
-            return (
-                <>
-                    {isDesktop &&
-                        (() => {
-                            const redirect_url = new URL(standalone_routes.personal_details);
-                            const urlParams = new URLSearchParams(window.location.search);
-                            const account_param = urlParams.get('account');
-                            const is_virtual = client?.is_virtual || account_param === 'demo';
+    const renderAccountSection = () => {
+        if (isAuthorizing) {
+            return <AccountsInfoLoader isLoggedIn isMobile={!isDesktop} speed={3} />;
+        } else if (activeLoginid) {
+            return (
+                <>
+                    {isDesktop &&
+                        (() => {
+                            const redirect_url = new URL(standalone_routes.personal_details);
+                            const urlParams = new URLSearchParams(window.location.search);
+                            const account_param = urlParams.get('account');
+                            const is_virtual = client?.is_virtual || account_param === 'demo';
 
-                            if (is_virtual) {
-                                redirect_url.searchParams.set('account', 'demo');
-                            } else if (currency) {
-                                redirect_url.searchParams.set('account', currency);
-                            }
-                            return (
-                                <Tooltip
-                                    as='a'
-                                    href={redirect_url.toString()}
-                                    tooltipContent={localize('Manage account settings')}
-                                    tooltipPosition='bottom'
-                                    className='app-header__account-settings'
-                                >
-                                    <StandaloneCircleUserRegularIcon className='app-header__profile_icon' />
-                                </Tooltip>
-                            );
-                        })()}
-                    <AccountSwitcher activeAccount={activeAccount} />
-                    {isDesktop &&
-                        (has_wallet ? (
-                            <Button
-                                className='manage-funds-button'
-                                has_effect
-                                text={localize('Manage funds')}
-                                onClick={() => {
-                                    let redirect_url = new URL(standalone_routes.wallets_transfer);
-                                    if (isGBAvailable && isGBLoaded) {
-                                        redirect_url = new URL(standalone_routes.recent_transactions);
-                                    }
-                                    if (currency) {
-                                        redirect_url.searchParams.set('account', currency);
-                                    }
-                                    window.location.assign(redirect_url.toString());
-                                }}
-                                primary
-                            />
-                        ) : (
-                            <Button
-                                primary
-                                onClick={() => {
-                                    const redirect_url = new URL(standalone_routes.cashier_deposit);
-                                    if (currency) {
-                                        redirect_url.searchParams.set('account', currency);
-                                    }
-                                    window.location.assign(redirect_url.toString());
-                                }}
-                                className='deposit-button'
-                            >
-                                {localize('Deposit')}
-                            </Button>
-                        ))}
-                </>
-            );
-        } else {
-            return (
-                <div className='auth-actions'>
-                    <Button
-                        tertiary
-                        onClick={async () => {
-                            window.location.href = 'https://oauth.deriv.com/oauth2/authorize?app_id=71895&l=EN&brand=deriv';
-                        }}
-                    >
-                        <Localize i18n_default_text='Log in' />
-                    </Button>
-                    <Button
-                        primary
-                        onClick={() => {
-                            window.open('https://track.deriv.com/_71lZpQSowCdB4VdSfJsOp2Nd7ZgqdRLk/1/', '_blank');
-                        }}
-                    >
-                        <Localize i18n_default_text='Sign up' />
-                    </Button>
-                </div>
-            );
-        }
-    };
+                            if (is_virtual) {
+                                redirect_url.searchParams.set('account', 'demo');
+                            } else if (currency) {
+                                redirect_url.searchParams.set('account', currency);
+                            }
+                            return (
+                                <Tooltip
+                                    as='a'
+                                    href={redirect_url.toString()}
+                                    tooltipContent={localize('Manage account settings')}
+                                    tooltipPosition='bottom'
+                                    className='app-header__account-settings'
+                                >
+                                    <StandaloneCircleUserRegularIcon className='app-header__profile_icon' />
+                                </Tooltip>
+                            );
+                        })()}
+                    <AccountSwitcher activeAccount={activeAccount} />
+                    {isDesktop &&
+                        (has_wallet ? (
+                            <Button
+                                className='manage-funds-button'
+                                has_effect
+                                text={localize('Manage funds')}
+                                onClick={() => {
+                                    let redirect_url = new URL(standalone_routes.wallets_transfer);
+                                    if (isGBAvailable && isGBLoaded) {
+                                        redirect_url = new URL(standalone_routes.recent_transactions);
+                                    }
+                                    if (currency) {
+                                        redirect_url.searchParams.set('account', currency);
+                                    }
+                                    window.location.assign(redirect_url.toString());
+                                }}
+                                primary
+                            />
+                        ) : (
+                            <Button
+                                primary
+                                onClick={() => {
+                                    const redirect_url = new URL(standalone_routes.cashier_deposit);
+                                    if (currency) {
+                                        redirect_url.searchParams.set('account', currency);
+                                    }
+                                    window.location.assign(redirect_url.toString());
+                                }}
+                                className='deposit-button'
+                            >
+                                {localize('Deposit')}
+                            </Button>
+                        ))}
+                </>
+            );
+        } else {
+            return (
+                <div className='auth-actions'>
+                    <Button
+                        tertiary
+                        onClick={async () => {
+                            window.location.href = 'https://oauth.deriv.com/oauth2/authorize?app_id=71895&l=EN&brand=deriv';
+                        }}
+                    >
+                        <Localize i18n_default_text='Log in' />
+                    </Button>
+                    <Button
+                        primary
+                        onClick={() => {
+                            window.open('https://track.deriv.com/_71lZpQSowCdB4VdSfJsOp2Nd7ZgqdRLk/1/', '_blank');
+                        }}
+                    >
+                        <Localize i18n_default_text='Sign up' />
+                    </Button>
+                </div>
+            );
+        }
+    };
 
-    return (
-        <Header
-            className={clsx('app-header', {
-                'app-header--desktop': isDesktop,
-                'app-header--mobile': !isDesktop,
-            })}
-        >
-            <Wrapper variant='left'>
-                {/* New logo section with proportional size increase */}
-                <div className='custom-logo-wrapper' style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                    <img src='/assets/bull-logo.png' alt='Bull Logo' style={{ height: '40px' }} /> {/* Increased size */}
-                    <img src='/assets/poweredbyderiv-badge.png' alt='Powered by Deriv' style={{ height: '24px' }} /> {/* Increased size */}
-                </div>
+    return (
+        <Header
+            className={clsx('app-header', {
+                'app-header--desktop': isDesktop,
+                'app-header--mobile': !isDesktop,
+            })}
+        >
+            <Wrapper variant='left'>
+                {/* New logo section with proportional size increase */}
+                <div className='custom-logo-wrapper' style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                       </div>
 
-                {isDesktop ? (
-                    <div className='mobile-menu'>
-                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
-                            <ArrowUpCircle className='mobile-menu__icon' />
-                            Withdraw
-                        </button>
-                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
-                            <ArrowDownCircle className='mobile-menu__icon' />
-                            Deposit
-                        </button>
-                        <button onClick={() => window.location.href = 'https://t.me/ProfitMaxTraderHub'}>
-                            <Mail className='mobile-menu__icon' />
-                            Contact
-                        </button>
-                    </div>
-                ) : (
-                    <div className='mobile-menu-icon'>
-                        <Menu
-                            onClick={() => {
-                                setIsMenuOpen(!isMenuOpen);
-                            }}
-                            className='mobile-menu-icon__button'
-                        />
-                    </div>
-                )}
+                {isDesktop ? (
+                    <div className='mobile-menu'>
+                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
+                            <ArrowUpCircle className='mobile-menu__icon' />
+                            Withdraw
+                        </button>
+                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
+                            <ArrowDownCircle className='mobile-menu__icon' />
+                            Deposit
+                        </button>
+                        <button onClick={() => window.location.href = 'https://t.me/ProfitMaxTraderHub'}>
+                            <Mail className='mobile-menu__icon' />
+                            Contact
+                        </button>
+                    </div>
+                ) : (
+                    <div className='mobile-menu-icon'>
+                        <Menu
+                            onClick={() => {
+                                setIsMenuOpen(!isMenuOpen);
+                            }}
+                            className='mobile-menu-icon__button'
+                        />
+                    </div>
+                )}
 
-                {isMenuOpen && !isDesktop && (
-                    <div className='mobile-menu' style={{ background: 'rgba(0, 0, 0, 0.7)', border: '2px solid red' }}>
-                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
-                            <ArrowUpCircle className='mobile-menu__icon' />
-                            Withdraw
-                        </button>
-                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
-                            <ArrowDownCircle className='mobile-menu__icon' />
-                            Deposit
-                        </button>
-                        <button onClick={() => window.location.href = 'https://t.me/ProfitMaxTraderHub'}>
-                            <Mail className='mobile-menu__icon' />
-                            Contact
-                        </button>
-                    </div>
-                )}
-            </Wrapper>
-            <Wrapper variant='right'>{renderAccountSection()}</Wrapper>
-        </Header>
-    );
+                {isMenuOpen && !isDesktop && (
+                    <div className='mobile-menu' style={{ background: 'rgba(0, 0, 0, 0.7)', border: '2px solid red' }}>
+                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
+                            <ArrowUpCircle className='mobile-menu__icon' />
+                            Withdraw
+                        </button>
+                        <button onClick={() => window.location.href = 'https://dm-pay.africa/'}>
+                            <ArrowDownCircle className='mobile-menu__icon' />
+                            Deposit
+                        </button>
+                        <button onClick={() => window.location.href = 'https://t.me/ProfitMaxTraderHub'}>
+                            <Mail className='mobile-menu__icon' />
+                            Contact
+                        </button>
+                    </div>
+                )}
+            </Wrapper>
+            <Wrapper variant='right'>{renderAccountSection()}</Wrapper>
+        </Header>
+    );
 });
 
 export default AppHeader;
